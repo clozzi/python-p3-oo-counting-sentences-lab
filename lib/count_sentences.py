80976d15df5060f57d10f7716ce1db11792dc178
Chris Lozzi <clozzi12@gmail.com>
2023-12-06 14:25:41 -0500

    all tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+  
+  def is_sentence(self):
+    if self._value.endswith("."):
+      return True
+    else:
+      return False
+
+  def is_question(self):
+    if self._value.endswith("?"):
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self._value.endswith("!"):
+      return True
+    else:
+      return False
+
+  def count_sentences(self):
+    replace_punc = self._value.replace('.', ';').replace('!',';').replace('?', ';')
+    split_sent = replace_punc.split(';')
+    not_empty = [sentence for sentence in split_sent if sentence]
+    return len(not_empty)
